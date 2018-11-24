98f8ba2101d066e51252f559e6785f6738a19dcc
pengwow <pengwow@hotmail.com>
2018-11-24 16:59:58 +0800

    update

--- home_application/views.py
+++ home_application/views.py
@@ -11,12 +11,12 @@ def home(request):
     """
     首页
     """
-    bk_token = request.COOKIES.get(settings.BK_COOKIE_NAME, None)
+    #bk_token = request.COOKIES.get(settings.BK_COOKIE_NAME, None)
 
 
     #return render_mako_context(request, '/home_application/home.html')
-    account = Account()
-    user_info = account.get_bk_user_info(bk_token)
+    #account = Account()
+    #user_info = account.get_bk_user_info(bk_token)
 
     return render_mako_context(request, '/home_application/index.html')
 
