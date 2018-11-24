7ab5945788540710e7066a50bd7f611c4bfadc69
pengwow <pengwow@hotmail.com>
2018-11-24 16:54:39 +0800

    update

--- home_application/views.py
+++ home_application/views.py
@@ -3,18 +3,22 @@
 from common.mymako import render_mako_context
 from account.accounts import Account
 from django.conf import settings
+from account.decorators import login_exempt
+import requests
 
+@login_exempt
 def home(request):
     """
     首页
     """
+    bk_token = request.COOKIES.get(settings.BK_COOKIE_NAME, None)
+
+
     #return render_mako_context(request, '/home_application/home.html')
     account = Account()
+    user_info = account.get_bk_user_info(bk_token)
 
-    is_ok,resp= account.get_bk_user_info(settings.APP_TOKEN)
-    # return account.logout(request)
-    print(resp)
-    return render_mako_context(request, '/home_application/index.html',resp)
+    return render_mako_context(request, '/home_application/index.html',**user_info)
 
 def dev_guide(request):
     """
--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
-# 平台验证用户登录态接口
-BK_LOGIN_VERIFY_URL = "%s/login/accounts/is_login/" % getattr(settings, 'BK_PAAS_INNER_HOST', settings.BK_PAAS_HOST)
-# 平台获取用户信息接口
-BK_GET_USER_INFO_URL = "%s/login/accounts/get_user/" % getattr(settings, 'BK_PAAS_INNER_HOST',
-                                                               settings.BK_PAAS_HOST)
-from account.http import http_get
-param = {'bk_token': 'b0850347-93ec-4d92-84db-7637ce6d6056'}
-result, resp = http_get(BK_GET_USER_INFO_URL, param)
-print(result, resp )
+host = 'https://paas-poc.o.qcloud.com'
+import requests
+app_url= host + '/api/c/compapi/v2/cc/search_business/'
+print(app_url)
+# 修改模块属性
+modify_module_properties = 'http://aospaas.travelsky.com/api/c/compapi/cc/modify_module_properties/'
+from settings import APP_ID
+{'bk_app_code':APP_ID}
+
+result = requests.post(app_url,
+                       headers={'Accept': 'application/json',
+                                'Content-Type': 'application/json', },
+                       data=None)
+
+
+
