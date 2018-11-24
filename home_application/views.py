b72326c84aa1e4b6cd748b35e0e9dbb77aad3b28
pengwow <pengwow@hotmail.com>
2018-11-24 17:28:48 +0800

    update

--- home_application/views.py
+++ home_application/views.py
@@ -8,26 +8,28 @@ def home(request):
     """
     首页
     """
-    bk_token = request.COOKIES.get('bk_token', None)
+    try:
+        bk_token = request.COOKIES.get('bk_token', None)
 
-    host = 'https://paas-poc.o.qcloud.com'
+        host = 'https://paas-poc.o.qcloud.com'
 
-    app_url = host + '/api/c/compapi/v2/cc/search_business/'
+        app_url = host + '/api/c/compapi/v2/cc/search_business/'
 
-    data = {"bk_app_code": 'liupeng',
-            "bk_token": bk_token,
-            "bk_app_secret": 'b0850347-93ec-4d92-84db-7637ce6d6056'}
+        data = {"bk_app_code": 'liupeng',
+                "bk_token": bk_token,
+                "bk_app_secret": 'b0850347-93ec-4d92-84db-7637ce6d6056'}
 
-    result = requests.post(app_url,
-                           headers={'Accept': 'application/json',
-                                    'Content-Type': 'application/json', },
-                           data=data)
-    import json
-    if result.status_code == 200:
-        content = json.loads(result.content)
-        content = content.get('data')
-        print(content.get('data'))
-
+        result = requests.post(app_url,
+                               headers={'Accept': 'application/json',
+                                        'Content-Type': 'application/json', },
+                               data=data)
+        import json
+        if result.status_code == 200:
+            content = json.loads(result.content)
+            content = content.get('data')
+            print(content.get('data'))
+    except:
+        print('aaaaaaaaaaa')
     return render_mako_context(request, '/home_application/index.html')
 
 
