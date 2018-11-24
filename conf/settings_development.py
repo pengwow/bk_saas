5ee9a481fc0a444b6c9d561f82f7d2d9354afe09
pengwow <pengwow@hotmail.com>
2018-11-24 15:33:27 +0800

    update

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -8,14 +8,22 @@ from settings import APP_ID
 # ===============================================================================
 # 数据库设置, 本地开发数据库设置
 # ===============================================================================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
+#         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+#         'USER': 'root',                        # 你的数据库user
+#         'PASSWORD': '123456',                        # 你的数据库password
+#         'HOST': '127.0.0.1',                   # 开发的时候，使用localhost
+#         'PORT': '3306',                        # 默认3306
+#     },
+# }
+#
+from settings import APP_ID, PROJECT_ROOT
+import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                        # 你的数据库user
-        'PASSWORD': '123456',                        # 你的数据库password
-        'HOST': '127.0.0.1',                   # 开发的时候，使用localhost
-        'PORT': '3306',                        # 默认3306
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
+    }
 }
-
--- conf/settings_production.py
+++ conf/settings_production.py
@@ -8,13 +8,21 @@ from settings import APP_ID
 # ===============================================================================
 # 数据库设置, 正式环境数据库设置
 # ===============================================================================
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
+#         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+#         'USER': 'root',                            # 你的数据库user
+#         'PASSWORD': 'bk@321',                        # 你的数据库password
+#         'HOST': '10.0.1.45',                   		   # 数据库HOST
+#         'PORT': '3306',                        # 默认3306
+#     },
+# }
+from settings import APP_ID, PROJECT_ROOT
+import os
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'bk@321',                        # 你的数据库password
-        'HOST': '10.0.1.45',                   		   # 数据库HOST
-        'PORT': '3306',                        # 默认3306
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
+    }
 }
