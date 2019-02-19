abc757e7534078e201a76f8d5d2d3d8cfda826b1
pengwow <pengwow@hotmail.com>
2019-02-19 09:22:07 +0800

    修改http协议参数 ssl

--- account/http.py
+++ account/http.py
@@ -15,6 +15,21 @@ from django.conf import settings
 from common.log import logger
 
 
+def decorator_http_request(func):
+    def return_wrapper(*args, **wkargs):
+        try:
+            import settings
+            verify = True
+            if 'DEVELOP' == settings.RUN_MODE:
+                verify = False
+            result = func(*args, verify=verify, **wkargs)
+            return result
+        except Exception as e:
+            logger.error(str(e.message))
+
+    return return_wrapper
+
+
 def _gen_header():
     headers = {
         "Content-Type": "application/json",
@@ -24,28 +39,33 @@ def _gen_header():
     return headers
 
 
-def _http_request(method, url, headers=None, data=None):
+@decorator_http_request
+def _http_request(method, url, headers=None, data=None, **kwargs):
     try:
         if method == "GET":
-            resp = requests.get(url=url, headers=headers, params=data)
+            resp = requests.get(url=url, headers=headers, params=data, **kwargs)
         elif method == "HEAD":
-            resp = requests.head(url=url, headers=headers)
+            resp = requests.head(url=url, headers=headers, **kwargs)
         elif method == "POST":
-            resp = requests.post(url=url, headers=headers, json=data)
+            resp = requests.post(url=url, headers=headers, json=data, **kwargs)
         elif method == "DELETE":
-            resp = requests.delete(url=url, headers=headers, json=data)
+            resp = requests.delete(url=url, headers=headers, json=data, **kwargs)
         elif method == "PUT":
-            resp = requests.put(url=url, headers=headers, json=data)
+            resp = requests.put(url=url, headers=headers, json=data, **kwargs)
         else:
             return False, None
+    # except Exception as e:
+    #     logger.exception("login http request error! type: %s, url: %s, data: %s" % (method, url, str(data)))
+    #     return False, None
     except requests.exceptions.RequestException:
         logger.exception("login http request error! type: %s, url: %s, data: %s" % (method, url, str(data)))
         return False, None
     else:
         if resp.status_code != 200:
             content = resp.content[:100] if resp.content else ''
-            logger.error("login http request error! type: %s, url: %s, data: %s, response_status_code: %s, response_content: %s"  # noqa
-                         % (method, url, str(data), resp.status_code, content))
+            logger.error(
+                "login http request error! type: %s, url: %s, data: %s, response_status_code: %s, response_content: %s"  # noqa
+                % (method, url, str(data), resp.status_code, content))
             return False, None
 
         return True, resp.json()
--- test.py
+++ test.py
@@ -2,24 +2,36 @@
 from django.conf import settings
 host = 'https://paas.blueking.com'
 import requests
-app_url= host + '/api/c/compapi/v2/cc/search_business/'
-print(app_url)
-user_url = host + '/api/c/compapi/v2/bk_login/get_user/'
-
 import json
-# 修改模块属性
-modify_module_properties = 'http://aospaas.travelsky.com/api/c/compapi/cc/modify_module_properties/'
-from settings import APP_ID
 
-data = {"bk_app_code": 'liupeng',
-        "bk_token":'a3S9jNPQEq7vEONoNGKLhvZXvMD8FmIS',
-        "bk_app_secret":'bce161ad-188b-4184-8232-a6e97de0cae5'}
-data = json.dumps(data)
+url = host + '/login/accounts/is_login/'
+data = {"bk_token": "JbFZhq8uiAD3VeujAqHcj-0owF-hN1uU1kSdOlfu8O0"}
 headers = {'Accept': 'application/json','Content-Type': 'application/json'}
-result = requests.post(app_url, headers=headers, data=data)
 
+result = requests.get(url, headers=headers, params=data,verify=False)
 if result.status_code == 200:
     content = json.loads(result.content)
     print(content)
+else:
+    print(result)
+# app_url= host + '/api/c/compapi/v2/cc/search_business/'
+# print(app_url)
+# user_url = host + '/api/c/compapi/v2/bk_login/get_user/'
+#
+# import json
+# # 修改模块属性
+# modify_module_properties = 'http://aospaas.travelsky.com/api/c/compapi/cc/modify_module_properties/'
+# from settings import APP_ID
+#
+# data = {"bk_app_code": 'liupeng',
+#         "bk_token":'a3S9jNPQEq7vEONoNGKLhvZXvMD8FmIS',
+#         "bk_app_secret":'bce161ad-188b-4184-8232-a6e97de0cae5'}
+# data = json.dumps(data)
+# headers = {'Accept': 'application/json','Content-Type': 'application/json'}
+# result = requests.post(app_url, headers=headers, data=data)
+#
+# if result.status_code == 200:
+#     content = json.loads(result.content)
+#     print(content)
 
 
